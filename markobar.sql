fc492a49be0e06ff24f20249a9ac94a75cb4e70c
IndroMan <indrodwi2@gmail.com>
2022-11-02 03:18:17 +0800

    POSTTEST 7

--- markobar.sql
+++ markobar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2022 at 08:17 PM
+-- Generation Time: Nov 01, 2022 at 08:17 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,9 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id`, `id_menu`, `nama_gambar`, `upload_on`, `file`) VALUES
-(5, 43, 'Martabak Telur', '2022-10-26 12:16:20', 'Martabak Telur.jfif');
+(5, 43, 'Martabak Telur', '2022-10-26 12:16:20', 'Martabak Telur.jfif'),
+(6, 44, 'AIW', '2022-11-01 11:35:48', 'AIW.jpg'),
+(7, 45, 'WP', '2022-11-01 11:36:44', 'WP.jpg');
 
 -- --------------------------------------------------------
 
@@ -60,7 +62,9 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id_menu`, `nama_menu`, `harga_menu`, `stok`) VALUES
-(43, 'Martabak Telur Ayam', 100000, 24);
+(43, 'Martabak Telur Ayam', 100000, 24),
+(44, 'Martabak T-rex', 200000, 12),
+(45, 'Martabak Manis', 120000, 12);
 
 -- --------------------------------------------------------
 
@@ -82,6 +86,26 @@ CREATE TABLE `pasok` (
 
 INSERT INTO `pasok` (`id_pasok`, `id_menu`, `tanggal_masuk`, `jumlah`, `total`) VALUES
 (4, 43, '2022-10-26', 12, 1200000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
+(4, 'ujang', '$2y$10$v3VUEiEor.A8uO/2YDj7Y./zS1HXnoZIK6bq1AcgO9o1minqa1W0G'),
+(5, 'indro', '$2y$10$XJUfXdfjoLlmN1LJVrb3H.G49Qb284jo/2FTFlBcOBdOgkoG6lYYm');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +132,12 @@ ALTER TABLE `pasok`
   ADD KEY `id_menu` (`id_menu`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -115,19 +145,25 @@ ALTER TABLE `pasok`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `pasok`
 --
 ALTER TABLE `pasok`
   MODIFY `id_pasok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
